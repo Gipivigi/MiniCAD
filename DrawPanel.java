c3b8f6138522098308b6fabdc9c5666d9c5c7c5a
Giacomo Vigilante <126689849+Gipivigi@users.noreply.github.com>
2024-04-07 20:01:45 +0200

    Aggiunta del disegno di un ovale e aggiustamenti alla logica del rettangolo

--- DrawPanel.java
+++ DrawPanel.java
@@ -9,8 +9,8 @@ public class DrawPanel extends JPanel implements MouseListener, MouseMotionListe
     ArrayList<Cerchio> circlesList=new ArrayList<>();
     ArrayList<Punto> pointsList=new ArrayList<>();
     ArrayList<Rettangolo> rectanglesList=new ArrayList<>();
-    int figure=0, xRect=0, yRect=0;
-    Boolean drawing=false;
+    int figure=0, xRect=0, yRect=0, xCirc=0, yCirc=0;
+    Boolean drawing=false, lastRectangle=false, lastCircle=false;
 
 
     int x1=0, y1=0, x2=0, y2=0;
@@ -40,6 +40,25 @@ public class DrawPanel extends JPanel implements MouseListener, MouseMotionListe
             g.drawRect(r.getPuntoIniziale().getX(),r.getPuntoIniziale().getY(),r.getWidth(),r.getHeight());
             xRect=0;
             yRect=0;
+            if(drawing==false && lastRectangle==true)
+                {
+                    rectanglesList.remove(rectanglesList.size()-1);
+                    lastRectangle=false;
+                }
+        }
+        for(int i=0; i<circlesList.size(); i++)
+        {
+            Cerchio c=new Cerchio(circlesList.get(i).getWidth(),circlesList.get(i).getHeight(),circlesList.get(i).getPuntoIniziale(),Color.BLACK);
+            g.setColor(c.getC());
+            g.drawOval(c.getPuntoIniziale().getX(),c.getPuntoIniziale().getY(),c.getWidth(),c.getHeight());
+            xCirc=0;
+            yCirc=0;
+            if(drawing==false && lastCircle==true)
+            {
+                circlesList.remove(circlesList.size()-1);
+                lastCircle=false;
+            }
+
         }
 
     }
@@ -72,7 +91,10 @@ public class DrawPanel extends JPanel implements MouseListener, MouseMotionListe
                         break;
                     case 2:
                         rectanglesList.add(new Rettangolo(0,0,new Punto(x1,y1,Color.BLACK),Color.BLACK));
-                        System.out.println();
+                        drawing=true;
+                        break;
+                    case 3:
+                        circlesList.add(new Cerchio(0,0,new Punto(x1,y1,Color.BLACK),Color.BLACK));
                         drawing=true;
                         break;
                     default:
@@ -88,6 +110,10 @@ public class DrawPanel extends JPanel implements MouseListener, MouseMotionListe
                 switch(figure)
                 {
                     case 1:
+                        if(!segmentsList.isEmpty())
+                        {
+                            segmentsList.remove(segmentsList.size()-1);
+                        }
                         segmentsList.add(new Segmento(new Punto(x1,y1, Color.BLACK), new Punto (x2,y2, Color.BLACK), Color.BLACK));
                         drawing=false;
                         repaint();
@@ -95,6 +121,13 @@ public class DrawPanel extends JPanel implements MouseListener, MouseMotionListe
                     case 2:
                         rectanglesList.add(new Rettangolo(xRect,yRect, new Punto(x1,y1,Color.BLACK),Color.BLACK));
                         drawing=false;
+                        lastRectangle=true;
+                        repaint();
+                        break;
+                    case 3:
+                        circlesList.add(new Cerchio(xCirc,yCirc, new Punto(x1,y1,Color.BLACK),Color.BLACK));
+                        drawing=false;
+                        lastCircle=true;
                         repaint();
                         break;
                     default:
@@ -133,17 +166,29 @@ public class DrawPanel extends JPanel implements MouseListener, MouseMotionListe
                     break;
                 case 2:
                     Rettangolo r = rectanglesList.get(rectanglesList.size() - 1);
-                    int width = Math.abs(x1 - e.getX());
-                    int height = Math.abs(y1 - e.getY());
-                    r.setWidth(width);
-                    r.setHeight(height);
+                    int widthRect = Math.abs(x1 - e.getX());
+                    int heightRect = Math.abs(y1 - e.getY());
+                    r.setWidth(widthRect);
+                    r.setHeight(heightRect);
                     r.getPuntoIniziale().setX(Math.min(x1, e.getX()));
                     r.getPuntoIniziale().setY(Math.min(y1, e.getY()));
                     xRect=r.puntoIniziale.getX();
                     yRect=r.puntoIniziale.getY();
                     repaint();
-
                     break;
+                case 3:
+                    Cerchio c = circlesList.get(circlesList.size() - 1);
+                    int widthCirc = Math.abs(x1 - e.getX());
+                    int heightCirc = Math.abs(y1 - e.getY());
+                    c.setWidth(widthCirc);
+                    c.setHeight(heightCirc);
+                    c.getPuntoIniziale().setX(Math.min(x1, e.getX()));
+                    c.getPuntoIniziale().setY(Math.min(y1, e.getY()));
+                    xCirc=c.puntoIniziale.getX();
+                    yCirc=c.puntoIniziale.getY();
+                    repaint();
+                    break;
+
                 default:
                     break;
             }
